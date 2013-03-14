bebd803b2ba8c48fface4865a41979bf5b3fe378
BobieAir <ahchic@hotmail.com>
2013-03-14 11:37:14 +0800

    initiate project

---
+++ VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by BobieAir on 13/3/14.
+//  Copyright (c) 2013年 BobieAir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoPlayerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VideoPlayerDemo
+//
+//  Created by BobieAir on 13/3/14.
+//  Copyright (c) 2013年 BobieAir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
